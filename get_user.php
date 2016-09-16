4e744f245fd306c750632e82b733bf72071d89d0
atanych <atanych@gmail.com>
2016-09-17 00:12:58 +0300

    add logger example

--- get_user.php
+++ get_user.php
@@ -1,9 +1,31 @@
 <?php
 require 'vendor/autoload.php';
+
+class Logger extends \Psr\Log\AbstractLogger
+{
+    public $log_file;
+
+    public function __construct($file) {
+        $this->log_file = $file;
+    }
+
+    public function log($level, $message, array $context = [])
+    {
+        file_put_contents($this->log_file, '[' . date("Y-m-d H:i:s") . '] php.' . $level . " " . $message . "\n", FILE_APPEND);
+    }
+}
 $at = '7a13fd1be0f953d2223d0306b032fd39bb9e9282b9c5a045827d460921065eb25fac4481722c4bf766937';
-$request = new \VkSdk\Users\UsersGet(null);
-$request->setUserId(11975057);
+$request = new \VkSdk\Users\UsersGet(null, new Logger("new_logger"));
+$request->setUserId(1025529);
 $request->setField('photo_200');
 $request->doRequest();
-$result = $request->getUsersInfo();
-print_r($result->getFirstName());
+//$result = $request->getUsersInfo();
+//print_r($result->getFirstName());
+//echo PHP_EOL;
+//
+//print_r($result->getLastName());
+//echo PHP_EOL;
+//
+//
+//print_r($result->getPhoto());
+//echo PHP_EOL;
